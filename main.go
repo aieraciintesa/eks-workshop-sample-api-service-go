747958fe6a77e14d6d989148a697179f167a329c
aieraciintesa <123178683+aieraciintesa@users.noreply.github.com>
2023-01-20 16:44:50 +0100

    Update main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Change code"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
